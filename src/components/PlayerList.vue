21f3125f1f6f47fa6224b12db5ad5dccc0bbc0a4
Pablo Nieto <panietoar@gmail.com>
2017-08-18 18:51:46 -0500

    Filtering player list

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <h3>All Players</h3>
-    <v-list two-line 
-          class="elevation-3">
-      <template v-for="player in players" >
-        <v-list-tile avatar 
-          :key="player.id" 
-          :to="{ name: 'playerDetail', params: { id: player.id } }">
+    <v-layout row>
+      <v-flex sm4 xs6 offset-xs5 offset-sm8>
+        <v-text-field label="search" v-model="searchTerm" append-icon="search" type="text" class="text-xs-right">
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-list two-line class="elevation-3">
+      <template v-for="player in playerList">
+        <v-list-tile avatar :key="player.id" :to="{ name: 'playerDetail', params: { id: player.id } }">
           <v-list-tile-avatar>
             <img :src="player.avatar ? player.avatar : defaultAvatar"></v-list-tile-avatar>
           </v-list-tile-avatar>
@@ -21,26 +24,32 @@
 </template>
 
 <script>
-import playerService from '../js/services/PlayerService.js';
+  import playerService from '../js/services/PlayerService.js';
 
-export default {
-  data(){
-    return {
-      players: [],
-      defaultAvatar: "public/img/avatar.png"
-    };
-  },
-  beforeMount() {
-    var vm = this;
-    playerService.getPlayerList().then(function(response) {
-      vm.players = response.data;
-    });
-  },
-  methods: {
-    getPlayers() {
+  export default {
+    data() {
+      return {
+        players: [],
+        defaultAvatar: "public/img/avatar.png",
+        searchTerm: ''
+      };
+    },
+    beforeMount() {
+      var vm = this;
+      playerService.getPlayerList().then(function (response) {
+        vm.players = response.data;
+      });
+    },
+    computed: {
+      playerList() {
+        return this.players.filter(player => {
+          return player.fullName.match(this.searchTerm)
+                  || player.teamName.match(this.searchTerm);
+        });
+      }
     }
   }
-}
+
 </script>
 
 <style lang="sass" scoped>
